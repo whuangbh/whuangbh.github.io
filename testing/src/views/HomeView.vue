bc186245ba76fdca4aae1a0929e70eb5c5ecaa79
Roy Huang <whuangbh@connect.ust.hk>
2025-05-03 23:56:17 +1000

    refactor

--- testing/src/views/HomeView.vue
+++ testing/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>Hello World!</div>
 </template>
+
+<style scoped></style>
